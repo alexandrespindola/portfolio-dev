1cb58bead874f5adcbf3c36a932b1cfcb1e2a568
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-12-11 17:33:04 +0100

    git backup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#40ea1e',
+				secondary: '#57cc94',
+				background: '#050505',
+			},
+		},
 	},
 	plugins: [],
 }
