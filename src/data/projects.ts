0916031676aa715ec678ddb49a3a7321158dee38
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-12-18 14:37:59 +0100

    renomeia projeto "SendNotes" para "MyReminders" e atualiza descrição, link, imagem e tags; adiciona projeto "SendNotes" com descrição e imagem atualizadas

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -47,11 +47,20 @@ const PROJECTS: Project[] = [
     github: "https://github.com/alexandrespindola/portfolio-dev",
   },
   {
-    title: "SendNotes - Tall Stack",
+    title: "MyReminders",
     description:
-      "SendNotes is a simple web application developed with the TALL Stack (Tailwind CSS, Alpine.js, Laravel and Livewire), allowing create notes and share them by email. The application uses Laravel for the backend, Livewire for dynamic updates, Tailwind CSS for responsive design and Alpine.js for interactivity on the client. It also uses Blade and WireUI, a powerful toolkit designed to revolutionize the development workflow. SendNotes is hosted on Fly.io, which offers a scalable and efficient infrastructure for web applications.",
+      "MyReminders is a web application developed from scratch with the TALL Stack (Tailwind CSS, Alpine.js, Laravel, and Livewire), allowing users to create reminders that will be automatically sent to their email. The application uses Laravel for the backend, Livewire for dynamic updates, Tailwind CSS for responsive design, and Alpine.js for interactivity on the client. It also uses Blade and WireUI, a powerful toolkit designed to revolutionize the development workflow. The app is hosted on Fly.io, which offers a scalable and efficient infrastructure for web applications, and uses Mailgun to send emails.",
+    link: "https://myreminder.fly.dev",
+    image: "https://assets-bunny-cdn.b-cdn.net/portfolio-dev/myreminders.webp",
+    tags: [TAGS.LARAVEL, TAGS.TAILWIND, TAGS.ALPINE, TAGS.LIVEWIRE, TAGS.WIREUI, TAGS.SUPABASE, TAGS.PHP, TAGS.FLY],
+    github: "https://github.com/alexandrespindola/reminders",
+  },
+  {
+    title: "SendNotes",
+    description:
+      'Developed with the TALL Stack (Tailwind CSS, Alpine.js, Laravel and Livewire), SendNotes allow create notes and share them by email. Hosted on Fly.io. Developed with the "The Complete TALL Stack Tutorial" by Josh Cirre',
     link: "https://sendnotes-tall.fly.dev",
-    image: "https://assets-bunny-cdn.b-cdn.net/portfolio-dev/sendnotes.webp",
+    image: "https://assets-bunny-cdn.b-cdn.net/portfolio-dev/sendnotes2.webp",
     tags: [TAGS.LARAVEL, TAGS.TAILWIND, TAGS.ALPINE, TAGS.LIVEWIRE, TAGS.WIREUI, TAGS.SUPABASE, TAGS.PHP, TAGS.FLY],
     github: "https://github.com/alexandrespindola/sendnotes-tall",
   },
